ddb2701a91d325d002f1ed63a515dfb2df0a7780
Daniel Sierra <danylang77@gmail.com>
2022-10-13 11:35:26 -0700

    A lot of changes in distance vector

--- DistanceVectorAlgorithm.java
+++ DistanceVectorAlgorithm.java
@@ -2,46 +2,116 @@ import java.io.*;
 import java.util.*;
 import java.util.concurrent.Semaphore;
 
+import javax.print.DocFlavor.STRING;
+
 public class DistanceVectorAlgorithm {
 
     // representa que nodo soy en la topologia
-    public String minodo = "";
+    public String myNode = "";
     // contiene todos los destinos [A,B,C]
-    public LinkedList<String> destinos = new LinkedList<String>();
+    public ArrayList<String> otherNodes = new ArrayList<String>();
     /* Distance Vector */
-    public HashMap<String, HashMap<String, HashMap<String, String>>> vecinos = new HashMap<String, HashMap<String, HashMap<String, String>>>();
-    public HashMap<String, HashMap<String, HashMap<String, String>>> dv = new HashMap<String, HashMap<String, HashMap<String, String>>>();
+    public HashMap<String, HashMap<String, HashMap<String, String>>> neighbours = new HashMap<String, HashMap<String, HashMap<String, String>>>();
+    public HashMap<String, HashMap<String, HashMap<String, String>>> distanceVectorHashMap = new HashMap<String, HashMap<String, HashMap<String, String>>>();
 
-    public String config = "";
-    public HashMap<String, HashMap<String, String>> hostVecinos = new HashMap<String, HashMap<String, String>>();
+    public String configFile = "";
+
+    public HashMap<String, HashMap<String, String>> hostNeighbours = new HashMap<String, HashMap<String, String>>();
     // referencia al log
-    // public LogProyecto log_p;
+    public Log log;
     // contiene la información de las rutas
-    public HashMap<String, String> datos = new HashMap<String, String>();
+    public HashMap<String, String> routeMap = new HashMap<String, String>();
     // flag que indica si hay cambios que transmitir
-    public Boolean hayCambios = false;
+    public Boolean changesFlag = false;
 
     // para modificaciones concurrentes en el Distance Vector
     // We'll start with java.util.concurrent.Semaphore.
     // We can use semaphores to limit the number of concurrent threads accessing a
     // specific resource.
-    public Semaphore sp = new Semaphore(1);
+    public Semaphore threadResource = new Semaphore(1);
 
     /* Para ver el estatus y el comportamiento */
-    public HashMap<String, Boolean> vecinoNotificado = new HashMap<String, Boolean>(); // Cliente
-    public HashMap<String, Boolean> vecinoEscuchando = new HashMap<String, Boolean>(); // Cliente
-    public HashMap<String, Boolean> vecinoConectado = new HashMap<String, Boolean>(); // Server
+    // Cliente
+    public HashMap<String, Boolean> neighbourNotify = new HashMap<String, Boolean>();
+    // Cliente
+    public HashMap<String, Boolean> neighbourListening = new HashMap<String, Boolean>();
+    // Server
+    public HashMap<String, Boolean> neighbourConnected = new HashMap<String, Boolean>();
 
-    public DistanceVectorAlgorithm() {
-
+    public DistanceVectorAlgorithm(Log log) {
+        this.log = log;
     }
 
     public void readConfigFile() {
-
+        try {
+            File file = new File("config.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            routeMap = new HashMap<String, String>();
+            String readline = "";
+            while ((readline = bufferedReader.readLine()) != null) {
+                String[] configData = readline.split(",");
+                if (configData.length == 1) {
+                    myNode = configData[0];
+                    continue;
+                }
+                routeMap.put(configData[0], configData[1]);
+                HashMap<String, String> host = new HashMap<String, String>();
+                host.put("ip", configData[2]);
+                if (configData.length > 3) {
+                    host.put("port", configData[3]);
+                } else {
+                    host.put("port", "9080");
+                }
+                hostNeighbours.put(configData[0], host);
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void initDistanceVectorAlgorithm() {
+    public void init() {
+        readConfigFile();
+        log.add("My Node ---> " + myNode);
+        distanceVectorAlgorithm(myNode, true);
+    }
 
+    public void distanceVectorAlgorithm(String node, Boolean firstTime) {
+
+        HashMap<String, HashMap<String, String>> costMap = new HashMap<String, HashMap<String, String>>();
+        HashMap<String, String> whereToGoMap = new HashMap<String, String>();
+
+        /* Agregar mi nodo a los destinos */
+        if (!this.otherNodes.contains(myNode)) {
+            this.otherNodes.add(myNode);
+        }
+        whereToGoMap.put("hop", node);
+        whereToGoMap.put("cost", "0");
+        costMap.put(node, whereToGoMap);
+
+        /* Agregar mis vecinos */
+        for (String nodeFromRouteMap : routeMap.keySet()) {
+            if (!this.otherNodes.contains(nodeFromRouteMap)) {
+                this.otherNodes.add(nodeFromRouteMap);
+            }
+            whereToGoMap = new HashMap<String, String>();
+            if (routeMap.get(nodeFromRouteMap).contains("99")) {
+                whereToGoMap.put("hop", "");
+            } else {
+                whereToGoMap.put("hop", nodeFromRouteMap);
+            }
+            whereToGoMap.put("cost", routeMap.get(nodeFromRouteMap));
+            costMap.put(nodeFromRouteMap, whereToGoMap);
+            /* Inicializar variables para control */
+            if (firstTime) {
+                this.neighbourConnected.put(nodeFromRouteMap, false);
+                this.neighbourNotify.put(nodeFromRouteMap, false);
+                this.neighbourListening.put(nodeFromRouteMap, false);
+            }
+        }
+        /* El primer Distance Vector */
+        this.distanceVectorHashMap.put(node, costMap);
     }
 
     public void stablishRoutes() {
--- Torrent.java
+++ Torrent.java
@@ -4,36 +4,42 @@ import java.io.InputStreamReader;
 public class Torrent {
     public static void main(String[] arg) throws Exception {
 
-        BufferedReader inConsole = new BufferedReader(new InputStreamReader(System.in));
-        String userInput = "";
-        while (!userInput.contains("exit")) {
-            System.out.println("Ingrese comando: ");
-            userInput = inConsole.readLine();
-            String[] commands = userInput.split(" ");
+        // Distance Vector Algorithm
+        Log logsDistanceVector = new Log("DV");
+        DistanceVectorAlgorithm distanceVectorAlgorithm = new DistanceVectorAlgorithm(logsDistanceVector);
+        distanceVectorAlgorithm.init();
 
-            // debe cumplir con esta estructura
-            // hacer una lista de comandos
+        // BufferedReader inConsole = new BufferedReader(new
+        // InputStreamReader(System.in));
+        // String userInput = "";
+        // while (!userInput.contains("exit")) {
+        // System.out.println("Ingrese comando: ");
+        // userInput = inConsole.readLine();
+        // String[] commands = userInput.split(" ");
 
-            // [0] = destiny
-            // [1] = name
-            // [2] = size
-            System.out.println(commands[0]);
-            // if (commands.length == 3) {
+        // // debe cumplir con esta estructura
+        // // hacer una lista de comandos
 
-            // } else if (commands.length == 1) {
-            // if (commands[0].contains("mostrar")) {
+        // // [0] = destiny
+        // // [1] = name
+        // // [2] = size
+        // System.out.println(commands[0]);
+        // // if (commands.length == 3) {
 
-            // } else if (commands[0].contains("printdv")) {
+        // // } else if (commands.length == 1) {
+        // // if (commands[0].contains("mostrar")) {
 
-            // } else {
-            // System.out.println("Comando no valido");
-            // continue;
-            // }
-            // } else {
-            // System.out.println("Comando no valido");
-            // continue;
-            // }
-        }
+        // // } else if (commands[0].contains("printdv")) {
+
+        // // } else {
+        // // System.out.println("Comando no valido");
+        // // continue;
+        // // }
+        // // } else {
+        // // System.out.println("Comando no valido");
+        // // continue;
+        // // }
+        // }
 
     }
 }

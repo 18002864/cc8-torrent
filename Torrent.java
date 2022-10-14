f9903e1a03a7fcb2197b6f0de08ffddf29881562
Victor <victor.amado@blautech.us>
2022-10-13 18:51:45 -0600

    Application Module

--- Torrent.java
+++ Torrent.java
@@ -1,39 +1,13 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
+
+import app.App;
 
 public class Torrent {
     public static void main(String[] arg) throws Exception {
 
-        BufferedReader inConsole = new BufferedReader(new InputStreamReader(System.in));
-        String userInput = "";
-        while (!userInput.contains("exit")) {
-            System.out.println("Ingrese comando: ");
-            userInput = inConsole.readLine();
-            String[] commands = userInput.split(" ");
-
-            // debe cumplir con esta estructura
-            // hacer una lista de comandos
-
-            // [0] = destiny
-            // [1] = name
-            // [2] = size
-            System.out.println(commands[0]);
-            // if (commands.length == 3) {
-
-            // } else if (commands.length == 1) {
-            // if (commands[0].contains("mostrar")) {
-
-            // } else if (commands[0].contains("printdv")) {
-
-            // } else {
-            // System.out.println("Comando no valido");
-            // continue;
-            // }
-            // } else {
-            // System.out.println("Comando no valido");
-            // continue;
-            // }
-        }
+        // Aplicacion
+        App app = new App();
+        app.start();
 
     }
 }
---
+++ utils/Constants.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Constants {
+    public static final Integer FORWARD_PORT = 1981;
+    public static final String MY_NODE = "A";
+
+}
